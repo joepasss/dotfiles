9f87788ac5f5188a96b81cb9e1c366aa836c16fd
joepasss <farwes1180@gmail.com>
2024-03-11 18:37:07 +0900

    change lsp-config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,20 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" },
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "clangd",
+          "cssls",
+          "eslint",
+          "emmet_ls",
+          "html",
+          "jsonls",
+          "marksman",
+          "mdx_analyzer",
+          "rust_analyzer",
+          "cmake",
+        }
       })
     end,
   },
@@ -20,15 +33,31 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
+
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
       lspconfig.tsserver.setup({
         capabilities = capabilities,
+        filetypes = {
+          "typescript",
+        },
       })
       lspconfig.html.setup({
         capabilities = capabilities,
       })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.jsonls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.clangd.setup {}
+      lspconfig.cmake.setup {}
+      lspconfig.eslint.setup {}
+      lspconfig.flow.setup {}
+      lspconfig.marksman.setup {}
+
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
