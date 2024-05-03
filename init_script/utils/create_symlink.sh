b4ab273d43bf828aa531cf42df88861699083eed
joepasss <farwes1180@gmail.com>
2024-05-03 10:34:11 +0900

    add check before remove file & change var name

--- init_script/utils/create_symlink.sh
+++ init_script/utils/create_symlink.sh
@@ -3,7 +3,17 @@
 function delete_symlink() {
   local dest=$1
 
-  rm -rf $dest
+  echo -e "\n\e[1m\e[31m$dest is already exist ...\e[0m"
+  echo -e "\e[1mWould you like to delete \"$dest\"? \e[33m[\e[32myes\e[33m/\e[31mno\e[33m]\e[0m"
+  read answer
+
+  if [ $answer = "yes" ] || [ $answer = "y" ] || [ $answer = "ye" ]; then
+    echo "remove $dest ..."
+    sudo rm -rf $dest
+  else
+    echo "exit ..."
+    exit 1
+  fi
 }
 
 function create_symlink_home() {
@@ -15,21 +25,21 @@ function create_symlink_home() {
   fi
 
   echo "create $filename symlink"
-  ln -s $file $HOME/
+  ln -s $filepath $HOME/
 }
 
 function create_symlink_config() {
-  local file=$1
-  local filename=$(basename "$file")
+  local filepath=$1
+  local filename=$(basename "$filepath")
   
   if [ ! -d $HOME/.config ]; then
     mkdir $HOME/.config/
   fi
 
   if [ -e $HOME/.config/$file ]; then
-    delete_symlink $HOME/.config/$file
+    delete_symlink $HOME/.config/$filepath
   fi
 
   echo "create $filename symlink"
-  ln -s $file $HOME/.config/
+  ln -s $filepath $HOME/.config/
 }
