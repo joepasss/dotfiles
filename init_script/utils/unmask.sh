4b2d093112fcfcfbdf1d46acc6583d99b9f72a7d
joepasss <farwes1180@gmail.com>
2024-05-03 11:10:59 +0900

    @fix

--- init_script/utils/unmask.sh
+++ init_script/utils/unmask.sh
@@ -7,13 +7,13 @@ function unmask_package() {
   local unmask_dir="/etc/portage/package.accept_keywords"
 
   if [ ! -d $unmask_dir ]; then
-    sudo mkdir -p /etc/portage/package.accept_keywords
+    sudo mkdir -p "$unmask_dir"
   fi
 
   if [ ! -f $unmask_dir/$name ]; then
     echo "unmask $name ..."
     
     sudo touch $unmask_dir/$name
-    echo "$unmask_string" | sudo tee -a $unmask_dir/$name
+    echo "$unmask_string" | sudo tee -a "$unmask_dir/$name"
   fi
 }
