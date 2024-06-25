f11a7f627a02d86c4660b0d18f0d5ff53a594393
joepasss@gentoo <joepasss@gmail.com>
2024-06-26 05:52:14 +0900

    fix neovim configurations (lsp)

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -8,16 +8,22 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-      require("mason").setup({
+      require("mason-lspconfig").setup({
         ensure_installed = {
-          "lua-ls",
-          "bash-language-server",
+          "snyk_ls",
+          "typos_lsp",
+          "bashls",
           "clangd",
-          "rust-analyzer",
-          "dockerfile-language-server",
-          "cmake-language-server",
-          "typescript-language-server",
-          "stylelint",
+          "cmake",
+          "dockerls",
+          "docker_compose_language_service",
+          "emmet_ls",
+          "html",
+          "jsonls",
+          "tsserver",
+          "lua_ls",
+          "somesass_ls",
+          "stylelint_lsp",
         },
       })
     end,
@@ -29,15 +35,16 @@ return {
       CMake = {
         filetypes = { "cmake", "CMakeLists.txt" },
       },
-      Shell = {
-        filetypes = { "pre-commit", "pre-push" },
-      },
     },
     config = function()
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.lua_ls.setup({
+      lspconfig.snyk_ls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.typos_lsp.setup({
         capabilities = capabilities,
       })
 
@@ -54,7 +61,7 @@ return {
         root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
       })
 
-      lspconfig.rust_analyzer.setup({
+      lspconfig.cmake.setup({
         capabilities = capabilities,
       })
 
@@ -62,11 +69,31 @@ return {
         capabilities = capabilities,
       })
 
-      lspconfig.cmake.setup({
+      lspconfig.docker_compose_language_service.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.jsonls.setup({
         capabilities = capabilities,
       })
 
       lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.somesass_ls.setup({
         capabilities = capabilities,
       })
 
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,27 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua" },
+      ensure_installed = {
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "c",
+        "cmake",
+        "css",
+        "dockerfile",
+        "gitignore",
+        "html",
+        "javascript",
+        "json",
+        "regex",
+        "rust",
+        "scss",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+      },
       auto_install = true,
       highlight = {
         enable = true,
