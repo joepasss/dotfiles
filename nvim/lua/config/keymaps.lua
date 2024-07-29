b25dfbf96439954a4d0035cd9de437b0b32ec4b1
joepasss@gentoo <joepasss@gmail.com>
2024-07-30 07:50:21 +0900

    add silent option & neotree toggle function

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,19 +6,17 @@ vim.keymap.set("v", "<leader>c", '"+y')
 vim.keymap.set("n", "<leader>cc", '"+yy')
 
 -- neo-tree toggle
-vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", { noremap = true })
-
--- mason open
-vim.keymap.set("n", "<C-m>", ":Mason<CR>", { noremap = true })
+vim.keymap.set("n", "<C-m>", ":Neotree toggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-n>", ":Neotree focus<CR>", { noremap = true, silent = true })
 
 -- format
-vim.keymap.set("n", "<C-f>", ":Format<CR>", { noremap = true })
-vim.keymap.set("n", "<C-S-f>", ":FormatWrite<CR>", { noremap = true })
+vim.keymap.set("n", "<C-f>", ":Format<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-f>", ":FormatWrite<CR>", { noremap = true, silent = true })
 
 -- lsp
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true })
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true })
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
 
 --lazygit
-vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
