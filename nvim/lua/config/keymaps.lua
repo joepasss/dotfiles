afe4a59ac4ac00432e6e775ac8051446244994ee
joepasss@gentoo <farwes1180@gmail.com>
2024-12-21 12:41:23 +0000

    add neo-tree toggle key

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 
 -- lazygit
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
+
+-- neo-tree toggle
+vim.keymap.set("n", "<C-m>", ":Neotree toggle<CR>", { noremap = true, silent = true })
